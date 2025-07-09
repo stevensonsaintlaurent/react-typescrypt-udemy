e50597905b462364429536a69aaad340dab2a68b
Stevenson <stevensonstlaurent08@gmail.com>
2025-07-09 11:12:39 -0700

    le jour du practice problem

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,7 +1,6 @@
-import { FetchTodosAction, Todo } from "../actions";
-import { ActionTypes } from "../actions/types";
+import { Todo, ActionTypes, Action } from "../actions";
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
